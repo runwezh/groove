8eec1f5182fe48ba02549fee0690fd3d8440eda5
Michelle McGhee <michmac202@gmail.com>
2023-04-20 12:59:17 -0700

    adjust quantize closes #24

--- src/utils/quantize.js
+++ src/utils/quantize.js
@@ -1,33 +1,22 @@
 const determineQuantizeValue = (beats) => {
-	const beatValues = [1 / 4, 1 / 2, 1];
-
-	const smallestDistanceBetweenTwoBeats = beats.reduce((current, acc) => {
-		const distances = beats.map((beat) => Math.abs(beat - current));
-		const minDistance = Math.min(...distances);
-		return minDistance < acc ? minDistance : acc;
-	}, 0);
-
-	const distancesToQuantizeValues = beatValues.map((value) =>
-		Math.abs(value - smallestDistanceBetweenTwoBeats)
-	);
-	const quantizeValue =
-		beatValues[
-			distancesToQuantizeValues.indexOf(Math.min(...distancesToQuantizeValues))
-		];
-	return quantizeValue;
-
-	// const secondValue = beats[1];
-
-	// if (!secondValue) return 1;
-
-	// const distances = beatValues.map((value) => Math.abs(value - secondValue));
-	// return beatValues[distances.indexOf(Math.min(...distances))];
+	// const beatValues = [1 / 4, 1 / 2, 1];
+	// const smallestDistanceBetweenTwoBeats = beats.reduce((current, acc) => {
+	// 	const distances = beats.map((beat) => Math.abs(beat - current));
+	// 	const minDistance = Math.min(...distances);
+	// 	return minDistance < acc ? minDistance : acc;
+	// }, 0);
+	// const distancesToQuantizeValues = beatValues.map((value) =>
+	// 	Math.abs(value - smallestDistanceBetweenTwoBeats)
+	// );
+	// const quantizeValue =
+	// 	beatValues[
+	// 		distancesToQuantizeValues.indexOf(Math.min(...distancesToQuantizeValues))
+	// 	];
+	// return quantizeValue;
 };
 
-const quantize = (beats) => {
-	const quantizeValue = determineQuantizeValue(beats);
-
-	return beats.map((beat) => Math.round(beat / quantizeValue) * quantizeValue);
+const quantize = (beats, value = 0.5) => {
+	return beats.map((beat) => Math.round(beat / value) * value);
 };
 
 export default quantize;
