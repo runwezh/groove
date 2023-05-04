b70dd77109e0ab4a243f9084149389e94d52fe4f
Michelle McGhee <michmac202@gmail.com>
2023-05-03 18:25:16 -0700

    update github pages

--- docs/_app/immutable/chunks/3-84aff079.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/demo/elements/_page.svelte-ce721a04.js";export{t as component};
--- docs/_app/immutable/chunks/4-7d6d9dd5.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/demo/fonts/_page.svelte-04632fdb.js";export{t as component};
